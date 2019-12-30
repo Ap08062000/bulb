76f87cc73d83f796d934cdd85080549d5a1a0012
Ap08062000 <59360490+Ap08062000@users.noreply.github.com>
2019-12-30 14:18:34 +0400

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,11 @@
+function turnOn(){
+    var x = document.getElementById('IMG');
+    x.src = 'https://www.w3schools.com/js/pic_bulbon.gif';
+}
+
+function turnOff(){
+    var x = document.getElementById('IMG');
+    x.src = 'https://www.w3schools.com/js/pic_bulboff.gif';
+}
+
+document.getElementById("demo").innerHTML = 11+20;
